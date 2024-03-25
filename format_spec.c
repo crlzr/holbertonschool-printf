89484bc1110dfbdf19cc4e097550ca982c554245
Carole <c.zenruffinen@hotmail.com>
2024-03-25 08:24:34 +0000

    format_spec.c

--- format_spec.c
+++ format_spec.c
@@ -14,15 +14,15 @@ int format_spec(const char *format, format_t get_opt[], va_list list)
 	int j;
 	int ret;
 	int count;
-
+	/* Loop through each character of format string */
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-		{
+		{	/* Loop through format_t array */
 			for (j = 0; get_opt[j].string != NULL; j++)
-			{
+			{	/* i + 1 = specifier, check if it matches symbol */
 				if (format[i + 1] == get_opt[j].string[0])
-				{
+				{	/* calls function pointed to by f*/
 					ret = get_opt[j].f(list);
 					if (ret == -1)
 						return (-1);
