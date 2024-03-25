873a8c7627800f5958b1650662b0d646ba03e50a
Carole <c.zenruffinen@hotmail.com>
2024-03-25 23:26:34 +0000

    main.h

--- main.h
+++ main.h
@@ -3,12 +3,10 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int _write_char(char c);
-int set_char(va_list list);
-int set_string(va_list list);
-int set_percent(va_list list __attribute__((unused)));
 
 /**
  * struct conversion - template for a structure
@@ -23,5 +21,8 @@ typedef struct conversion
 } format_t;
 
 int format_spec(const char *format, format_t get_opt[], va_list list);
+int set_char(va_list list);
+int set_string(va_list list);
+int set_percent(va_list list __attribute__((unused)));
 
 #endif
