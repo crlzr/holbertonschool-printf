7dbb3dab1659cb320f3ced847be342df4af7c48b
Carole <c.zenruffinen@hotmail.com>
2024-03-25 04:54:43 +0000

    main_functions.c

---
+++ main_functions.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * set_char - print a character
+ * @list: list of characters
+ * Return: Will return the amount of characters printed
+ */
+
+int set_char(va_list list)
+{
+	_write_char(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * set_string - print a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed
+ */
+int set_string(va_list list)
+{
+	int index;
+	char *string;
+
+	string = va_arg(list, char *);
+	if (string == NULL)
+		string = "(null)";
+	for (index = 0; string[index] != '\0'; index++)
+		_write_char(string[index]);
+	return (index);
+}
+
+/**
+ * set_percent - print a percentage
+ * @list: list of arguments
+ * Return: Will return the number of characters printed
+ */
+
+int set_percent(va_list list __attribute__((unused)))
+{
+	_write_char('%');
+	return (1);
+}
+
