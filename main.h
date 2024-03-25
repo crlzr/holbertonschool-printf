0f4ae3644cd4096c478c8fe42e5529b7857c9462
Carole <c.zenruffinen@hotmail.com>
2024-03-25 04:51:36 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int set_char(va_list);
+int set_string(va_list);
+int set_percent(va_list);
+int _write_char(char c);
+
+/**
+ * struct conversion - template for a structure
+ * @string: the operator
+ * @f: pointer to a function
+ */
+
+typedef struct conversion
+{
+	char *string;
+	int (*f)(va_list);
+
+} format_t;
+
+int format_spec(const char *format, format_t get_opt[], va_list list);
+#endif
