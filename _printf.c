1520ce9a3cfbfd5e2a04f34fcdb358cb300b84a2
Carole <c.zenruffinen@hotmail.com>
2024-03-26 00:15:42 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
+	int i = 0;
 
 	format_t get_opt[] = {
 	{"c", set_char},
@@ -27,9 +27,54 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(list, format);
-
-	i = format_spec(format, get_opt, list);
-
 	va_end(list);
 	return (i);
+}
+
+/* 
+ * Code for format specifier
+ */
+
+int format_spec(const char *format, format_t get_opt[], va_list list)
+{
+        int i;
+        int j;
+        int ret;
+        int count = 0;
+        /* Loop through each character of format string */
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%')
+                {       /* Loop through format_t array */
+                        for (j = 0; get_opt[j].string != NULL; j++)
+                        {       /* i + 1 = specifier, check if it matches symbol */
+                                if (format[i + 1] == get_opt[j].string[0])
+                                {       /* calls function pointed to by f*/
+                                        ret = get_opt[j].f(list);
+                                        if (ret == -1)
+                                                return (-1);
+                                        count += ret;
+                                        break;
+                                }
+                        }
+                        if (get_opt[j].string == NULL && format[i + 1] != ' ')
+                        {
+                                if (format[i + 1] != '\0')
+                                {
+                                        _write_char(format[i]);
+                                        _write_char(format[i + 1]);
+                                        count += 2;
+                                }
+                                else
+                                        return (-1);
+                        }
+                        i++;
+                }
+                else
+                {
+                        _write_char(format[i]);
+                        count++;
+                }
+        }
+        return (count);
 }
