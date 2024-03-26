1c1cf3cd74ebbbf971c37cb85966bbef79a0f418
Carole <c.zenruffinen@hotmail.com>
2024-03-26 00:44:35 +0000

    main.h

--- main.h
+++ main.h
@@ -24,7 +24,6 @@ typedef struct conversion
 
 int _printf(const char *format, ...);
 int _write_char(char c);
-int format_spec(const char *format, format_t get_opt[], va_list list);
 int set_char(va_list *);
 int set_string(va_list *);
 int set_percent(va_list *);
